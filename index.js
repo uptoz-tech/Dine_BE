00afb3db22c0eca8a596d482a76141bda2f6f817
Sooriya N <sooriya.n@iorta.in>
2023-01-31 22:40:29 +0530

    structure added

---
+++ index.js
@@ -0,0 +1,33 @@
+require('dotenv').config({ path: __dirname + '/config/.env' })
+const express = require('express')
+require('./config/db')
+// const tableRouter = require('./routers/table')
+
+const app = express()
+app.use(express.json())
+
+const port = process.env.PORT || 7795
+// app.use(cors())
+
+app.use(function (req, res, next) {
+    const allowedOrigins = ["http://localhost:4200", "http://localhost:4300"];
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+        res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, PATCH, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+    next();
+})
+
+// app.use(tableRouter);
+const server = app.listen(port, () => {
+    console.log("Dine Running on : localhost", process.env.PORT);
+})
+
+
+process.on('unhandledRejection', (err, Promise) => {
+    console.log(`Error: ${err.message}`);
+    server.close(() => process.exit(1));
+});
